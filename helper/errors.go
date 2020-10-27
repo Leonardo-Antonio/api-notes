ce80a5b44530026bf6d6e2ed030906ccc0e87613
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-10-27 11:27:17 -0500

    erroros for user

--- helper/errors.go
+++ helper/errors.go
@@ -6,4 +6,12 @@ var (
 	ErrRowsAffected = errors.New("could not get affected rows")
 	ErrInsertionFailed = errors.New("insertion failed")
 	ErrDataInvalid = errors.New("the data entered is not valid")
+	ErrEmailInvalid = errors.New("the email entered is invalid")
+	ErrPasswordNotSecure = errors.New("the password entered is not secure")
+	ErrNickNameIvalid = errors.New("the nick name is invalid")
+)
+
+var (
+	ERROR = "error"
+	MESSAGE = "message"
 )
